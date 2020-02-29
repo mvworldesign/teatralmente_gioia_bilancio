44578cfa280a18f63d73d7d5cb1aaddf0831dd80
mattialeonardoangelillo <mattia.angelillo@mvworldesign.com>
2020-02-29 09:16:41 +0100

    Remove obsolete code Added comments

--- frontend/views/categoria/index.php
+++ frontend/views/categoria/index.php
@@ -1,5 +1,10 @@
 <?php
-
+/**
+ * Categorie, index page
+ * 
+ * @author Mattia Leonardo Angelillo
+ * @version 1.0
+ */
 use yii\helpers\Html;
 use yii\grid\GridView;
 use yii\bootstrap\Tabs;
@@ -59,33 +64,3 @@ $this->params['breadcrumbs'][] = $this->title;
     ],
 ]);
 ?>
-
-<!--
-<div class="categoria-index">
-
-    <h1><?= Html::encode($this->title) ?></h1>
-
-    <p>
-        <?= Html::a(Yii::t('app', 'Create Categoria'), ['create'], ['class' => 'btn btn-success']) ?>
-    </p>
-
-    <?php // echo $this->render('_search', ['model' => $searchModel]); ?>
-
-    <?= GridView::widget([
-        'dataProvider' => $dataProvider,
-        'filterModel' => $searchModel,
-        'columns' => [
-            ['class' => 'yii\grid\SerialColumn'],
-
-            'id',
-            'categoria',
-            'data_creazione',
-            'ultima_modifica',
-
-            ['class' => 'yii\grid\ActionColumn'],
-        ],
-    ]); ?>
-
-
-</div>
--->
